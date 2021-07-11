849ed2b4f326039a2dbde836caa835e732e384cf
Shuvo Nasir <shuvonasir028@gmail.com>
2021-07-11 18:53:01 +0600

    create a list of rgb color tuples using the colorgram library

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+import turtle as t
+import random as rd
+import colorgram as cg
 
+tim = t.Turtle()
+t.colormode(255)
+screen = t.Screen()
+
+# Extracting colors from the image
+colors = cg.extract("image.jpg", 10)
+
+# Creating a list of tuples of rgb colors
+rgb_colors = []
+for each_rgb in range(len(colors)):
+    color = []
+    for each_color in range(3):
+        color.append(colors[each_rgb].rgb[each_color])
+    color_tuple = tuple(color)
+    rgb_colors.append(color_tuple)
+
+screen.exitonclick()
