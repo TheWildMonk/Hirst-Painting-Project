790372593ed40eb8b4aa201cb14c4393454fac64
Shuvo Nasir <shuvonasir028@gmail.com>
2021-07-12 09:46:33 +0600

    update initial version of the project

--- initial_version.py
+++ initial_version.py
@@ -7,11 +7,6 @@ color_list = [(241, 222, 86), (35, 98, 185), (86, 174, 218),
               (20, 55, 146), (59, 119, 64), (118, 226, 184), (71, 30, 43), (135, 216, 233), (238, 158, 217),
               (41, 172, 183), (29, 41, 84), (242, 175, 152), (162, 165, 235), (90, 30, 22)]
 
-# Setting turtles attributes
-tim = t.Turtle()
-t.colormode(255)
-tim.speed(50)
-
 # Setting screen attributes
 screen = t.Screen()
 screen.canvwidth = 500
@@ -20,6 +15,12 @@ screen.canvheight = 500
 # Resetting the turtle world coordinates
 screen.reset()
 screen.setworldcoordinates(-50, -50, 500, 500)
+
+# Setting turtles attributes
+tim = t.Turtle()
+t.colormode(255)
+tim.speed("fastest")
+tim.hideturtle()
 
 
 # Function to create the art in a line
